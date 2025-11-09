37aa10ed576ee224096836d9b6f09a9ba5b89960
FionaSeahShuHui1998 <fionaseah98@gmail.com>
2025-11-09 16:34:26 +0800

    feat: link footer icons on index.html

---
+++ js/footer.js
@@ -0,0 +1,20 @@
+const emailLinks = document.querySelectorAll(".emailLink");
+emailLinks.forEach((link) => {
+  link.href = "mailto:fionaseah98@gmail.com";
+});
+
+const instagramLinks = document.querySelectorAll(".instagramLink");
+instagramLinks.forEach((link) => {
+  link.href =
+    "https://www.instagram.com/fionaseahshuhui2606?igsh=MXN5ZTJ6a2Jpd25qNw%3D%3D&utm_source=qr";
+});
+
+const linkedInLinks = document.querySelectorAll(".linkedInLink");
+linkedInLinks.forEach((link) => {
+  link.href = "https://www.linkedin.com/in/fiona-seah-shu-hui/";
+});
+
+const gitHubLinks = document.querySelectorAll(".gitHubLink");
+gitHubLinks.forEach((link) => {
+  link.href = "https://github.com/FionaSeahShuHui1998";
+});
