1bac430e36aeb4f6ba52dfd0b351763b55d0ce1e
FionaSeahShuHui1998 <fionaseah98@gmail.com>
2025-11-18 19:11:09 +0800

    feat: create introduction content area for contact page

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,29 @@
+const phoneLinks = document.querySelectorAll(".phoneLink");
+phoneLinks.forEach((link) => {
+  link.href = "tel:+6582749231";
+});
+
+const emailLinks = document.querySelectorAll(".emailLink");
+emailLinks.forEach((link) => {
+  link.href = "mailto:fionaseah98@gmail.com";
+});
+
+const instagramLinks = document.querySelectorAll(".instagramLink");
+instagramLinks.forEach((link) => {
+  link.href =
+    "https://www.instagram.com/fionaseahshuhui2606?igsh=MXN5ZTJ6a2Jpd25qNw%3D%3D&utm_source=qr";
+});
+
+const linkedInLinks = document.querySelectorAll(".linkedInLink");
+linkedInLinks.forEach((link) => {
+  link.href = "https://www.linkedin.com/in/fiona-seah-shu-hui/";
+});
+
+const gitHubLinks = document.querySelectorAll(".gitHubLink");
+gitHubLinks.forEach((link) => {
+  link.href = "https://github.com/FionaSeahShuHui1998";
+});
+
 const countryCodes = [
   { code: "+65", name: "Singapore" },
   { code: "+60", name: "Malaysia" },
